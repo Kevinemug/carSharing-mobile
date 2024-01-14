fe0c2abc909d27daeaa9612f01da5d3ad87dfb9b
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2024-01-14 14:15:20 +0200

    feat: remove page title component

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,46 +1,17 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import colors from '../config/colors';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+// import React from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
+// import colors from '../config/colors';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
 
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
 
-export default  PageTitle = ({ title }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-};
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 50,
-    backgroundColor: colors.secondary, // Consider using a gradient or image here    shadowColor: '#fff',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.8,
-    shadowRadius: 2,
-    elevation: 5,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'center',
-    textTransform: 'uppercase', // Adds a dramatic effect
-    letterSpacing: 2, // Increase letter spacing
-  },
-});
+// export default  PageTitle = ({ title }) => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>{title}</Text>
+//     </View>
+//   );
+// };
 
