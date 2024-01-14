0760fe1109ce5dbe281051ae4c192b51c0274712
Kevinemug <98740834+Kevinemug@users.noreply.github.com>
2024-01-14 14:14:08 +0200

    feat: remove unnecessary imports

--- src/components/AppText.jsx
+++ src/components/AppText.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text,StyleSheet } from 'react-native'
+import { Text,StyleSheet,Platform } from 'react-native'
 function AppText({children,style}) {
     return (
 <Text style={[styles.text,style]}>{children }</Text>
