e5a8d281a7151aded54a290d145ca9d0b5761963
nioctave <octavenishimwe82@gmail.com>
2024-01-18 21:04:40 +0200

    feat: final api changes

--- App.js
+++ App.js
@@ -12,7 +12,7 @@ export default function App() {
   return (
     <>
 <NavigationContainer>
-      <Stack.Navigator initialRouteName="AllCars">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="SignUp" component={SignUpPage} />
         <Stack.Screen name="Login" component={LoginPage} />
         <Stack.Screen name="AllCars" component={AllCarsScreen} />
